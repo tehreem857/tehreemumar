e2b5cd6499259ac691a5186e16c35e1dff3bbdbc
tehreem857 <136783529+tehreem857@users.noreply.github.com>
2026-09-05 04:18:40 +0500

    Restore real update_real_screenshot_tags.py

--- update_real_screenshot_tags.py
+++ update_real_screenshot_tags.py
@@ -3,12 +3,15 @@ import re
 with open('projects.html', 'r', encoding='utf-8') as f:
     html = f.read()
 
-# Update image cache version tags to v=5.0
-html = re.sub(r'images/project_resume_builder\.png\?v=\d+\.\d+', 'images/project_resume_builder.png?v=5.0', html)
-html = re.sub(r'images/project_tale_weave\.png\?v=\d+\.\d+', 'images/project_tale_weave.png?v=5.0', html)
-html = re.sub(r'images/project_jewelry_store\.png\?v=\d+\.\d+', 'images/project_jewelry_store.png?v=5.0', html)
+# Update image cache version tags to v=6.0 across all cards
+html = re.sub(r'images/project_resume_builder\.png\?v=\d+\.\d+', 'images/project_resume_builder.png?v=6.0', html)
+html = re.sub(r'images/project_tale_weave\.png\?v=\d+\.\d+', 'images/project_tale_weave.png?v=6.0', html)
+html = re.sub(r'images/project_jewelry_store\.png\?v=\d+\.\d+', 'images/project_jewelry_store.png?v=6.0', html)
+html = re.sub(r'images/project_closebot_ai\.png\?v=\d+\.\d+', 'images/project_closebot_ai.png?v=6.0', html)
+html = re.sub(r'images/project_ghl_pipeline\.png\?v=\d+\.\d+', 'images/project_ghl_pipeline.png?v=6.0', html)
+html = re.sub(r'images/project_ai_support\.png\?v=\d+\.\d+', 'images/project_ai_support.png?v=6.0', html)
 
 with open('projects.html', 'w', encoding='utf-8') as f:
     f.write(html)
 
-print("Updated image tags in projects.html with v=5.0!")
+print("Updated image tags in projects.html with v=6.0!")
