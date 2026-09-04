31bd35fdefbb4440966f6a8f60eb59fe11dff64b
tehreem857 <136783529+tehreem857@users.noreply.github.com>
2026-09-05 04:12:09 +0500

    Restore real update_real_screenshot_tags.py

--- update_real_screenshot_tags.py
+++ update_real_screenshot_tags.py
@@ -3,12 +3,12 @@ import re
 with open('projects.html', 'r', encoding='utf-8') as f:
     html = f.read()
 
-# Update image cache version tags to v=4.0
-html = re.sub(r'images/project_resume_builder\.png\?v=\d+\.\d+', 'images/project_resume_builder.png?v=4.0', html)
-html = re.sub(r'images/project_tale_weave\.png\?v=\d+\.\d+', 'images/project_tale_weave.png?v=4.0', html)
-html = re.sub(r'images/project_jewelry_store\.png\?v=\d+\.\d+', 'images/project_jewelry_store.png?v=4.0', html)
+# Update image cache version tags to v=5.0
+html = re.sub(r'images/project_resume_builder\.png\?v=\d+\.\d+', 'images/project_resume_builder.png?v=5.0', html)
+html = re.sub(r'images/project_tale_weave\.png\?v=\d+\.\d+', 'images/project_tale_weave.png?v=5.0', html)
+html = re.sub(r'images/project_jewelry_store\.png\?v=\d+\.\d+', 'images/project_jewelry_store.png?v=5.0', html)
 
 with open('projects.html', 'w', encoding='utf-8') as f:
     f.write(html)
 
-print("Updated image tags in projects.html with v=4.0!")
+print("Updated image tags in projects.html with v=5.0!")
